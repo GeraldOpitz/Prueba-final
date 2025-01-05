25b3683116daab4fb1b393d67d46014bfc99d682
gopitz <geraldopitz@gmail.com>
2025-01-05 18:08:35 -0300

    deleted secrets

---
+++ Terraform/terraform-1/my-terraform-project/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2" {
+  source = "./modules/ec2"
+  instance_type = var.instance_type
+  ami_id = var.ami_id
+}
+
+module "lambda" {
+  source = "./modules/lambda"
+  SNS_TOPIC_ARN = module.sns.topic_arn
+}
+
+module "sg" {
+  source = "./modules/sg"
+}
+
+module "sns" {
+  source = "./modules/sns"
+}
+
+module "sqs" {
+  source = "./modules/sqs"
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+}
