25127b1eea08170ff03ff9ec70c5fdff45a4ed1a
gopitz <geraldopitz@gmail.com>
2025-01-05 23:48:33 -0300

    fixed variables 3

--- Terraform/terraform-1/my-terraform-project/modules/ec2/main.tf
+++ Terraform/terraform-1/my-terraform-project/modules/ec2/main.tf
@@ -1,9 +1,14 @@
 module "sg" {
   source = "../sg"
 }
+
+module "vpc" {
+  source = "../vpc"
+}
 resource "aws_instance" "prueba_instance" {
   ami = var.ami_id
   instance_type = var.instance_type
+  subnet_id = module.vpc.public_subnet_id
   security_groups = [module.sg.web_sg_name]
 
   tags = {
--- Terraform/terraform-1/my-terraform-project/modules/vpc/main.tf
+++ Terraform/terraform-1/my-terraform-project/modules/vpc/main.tf
@@ -1,5 +1,10 @@
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "my_vpc"
+  }
 }
 
 resource "aws_subnet" "public_subnet" {
@@ -7,12 +12,18 @@ resource "aws_subnet" "public_subnet" {
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone = "us-east-1a"
+  tags = {
+    Name = "public_subnet"
+  }
 }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id = aws_vpc.main_vpc.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1a"
+  tags = {
+    Name = "private_subnet"
+  }
 }
 
 resource "aws_route_table" "public_rt" {
